2b909be27f38cd417f4f31a6d170b42a167efd77
AdenWhitworth <aden.whitworth@gmail.com>
2024-07-02 08:21:24 -0700

    created basic node.js express app connected to a mysql database

--- database.js
+++ database.js
@@ -2,12 +2,17 @@ import mysql from "mysql2";
 import dotenv from "dotenv";
 dotenv.config();
 
-export const pool = mysql.createPool({
+const pool = mysql.createPool({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE
 }).promise();
+
+export async function getNotes() {
+    const [rows] = await pool.query("SELECT * FROM notes");
+    return rows;
+}
 
 export async function getNote(id){
     const [rows] = await pool.query(`
@@ -24,11 +29,11 @@ console.log(notes);
 */
 
 
-export async function createNote(title, content){
+export async function createNote(title, contents){
     const [result] = await pool.query(`
         INSERT INTO notes (title, contents)
         VALUES(?, ?)
-    `, [title,content]);
+    `, [title,contents]);
 
     const id = result.insertId;
 
